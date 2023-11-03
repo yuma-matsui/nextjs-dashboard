1d6449090814ff1055abec3290da29a7a0808c93
Yuma Matsui <oshino.iiyo@gmail.com>
2023-11-03 18:04:28 +0900

    🐛 Typo修正

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
 import { Inter, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
-export const lustiana = Lusitana({
+export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700']
 })
