5a8a7ae35cbcf1bc2263a0a258d77f4a13282801
Yuma Matsui <oshino.iiyo@gmail.com>
2023-11-03 16:24:16 +0900

    ✨ next/fontの導入

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lustiana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
