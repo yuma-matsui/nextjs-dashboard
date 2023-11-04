75e39333b94f18be4debf1340a53289b7876d5f1
Yuma Matsui <oshino.iiyo@gmail.com>
2023-11-04 19:04:35 +0900

    ✨ Chapter15 done.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { signIn } from '@/auth'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
@@ -109,4 +110,18 @@ export async function deleteInvoice(id: string) {
     throw new Error('')
   }
   revalidatePath('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin'
+    }
+    throw error
+  }
 }
